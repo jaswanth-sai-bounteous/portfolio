02b88596956dac78d780ebe8b80659f321e11874
Panchumarthi Jaswanth Sai <panchumarthi.sai@bounteous.com>
2026-02-03 17:43:36 +0530

    Fresh initial commit with all portfolio files

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
  function myFunction() {
     alert("function is working");
 }
+
+const themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  
+  if(document.body.classList.contains("dark")) {
+    themeToggleBtn.textContent = "☀️"; 
+  } else {
+    themeToggleBtn.textContent = "🌙"; 
+  }
+});
